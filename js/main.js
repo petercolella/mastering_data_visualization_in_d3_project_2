8980ea801a6635f67cf436d63513e5d27897ae57
Peter Colella <rsy0@cdc.gov>
2022-11-28 12:33:52 -0500

    updates main.js with steps 3 - 7 of instructions

--- js/main.js
+++ js/main.js
@@ -33,8 +33,25 @@ g.append("text")
   .attr("text-anchor", "middle")
   .text("Life Expectancy (Years)");
 
+const year = g
+  .append("text")
+  .attr("class", "year")
+  .attr("x", WIDTH)
+  .attr("y", HEIGHT - 10)
+  .attr("font-size", "40px")
+  .attr("text-anchor", "end");
+
+const x = d3.scaleLog();
+const y = d3.scaleLinear();
+const r = d3.scaleLinear();
+const color = d3.scaleOrdinal(d3.schemePastel1);
+
+const xAxisGroup = g.append("g").attr("class", "x axis").attr("transform", `translate(0, ${HEIGHT})`);
+const yAxisGroup = g.append("g").attr("class", "y axis");
+
+let i = 0;
+
 d3.json("data/data.json").then(function (data) {
-  console.log(data);
   const filteredData = data.map((d) => {
     const obj = d;
     return {
@@ -42,24 +59,42 @@ d3.json("data/data.json").then(function (data) {
       countries: obj.countries.filter((c) => c.income && c.life_exp && c.population),
     };
   });
-  console.log(filteredData[0].countries);
 
-  const x = d3.scaleLog().domain([100, 150000]).range([0, WIDTH]);
+  d3.interval(() => {
+    i++;
+    if (i === filteredData.length) i = 0;
+    update(filteredData[i]);
+  }, 100);
 
-  const y = d3.scaleLinear().domain([0, 90]).range([0, HEIGHT]);
+  update(filteredData[i]);
+});
+
+function update(data) {
+  year.text(data.year);
+  const t = d3.transition().duration(100);
+
+  x.domain([100, 150000]).range([0, WIDTH]);
+  y.domain([0, 90]).range([HEIGHT, 0]);
+  r.domain([0, d3.max(data.countries, (d) => Math.sqrt(d.population / Math.PI))]).range([5, 25]);
+  color.domain(Array.from(new Set(data.countries.map((c) => c.continent))).sort());
 
   const xAxisCall = d3.axisBottom(x).tickValues([400, 4000, 40000]).tickFormat(d3.format("$"));
-  g.append("g").attr("class", "x axis").attr("transform", `translate(0, ${HEIGHT})`).call(xAxisCall);
+  xAxisGroup.transition(t).call(xAxisCall);
 
   const yAxisCall = d3.axisLeft(y);
-  g.append("g").attr("class", "y axis").call(yAxisCall);
+  yAxisGroup.transition(t).call(yAxisCall);
 
-  g.selectAll("circle")
-    .data(filteredData[0].countries)
+  const circles = g.selectAll("circle").data(data.countries, (d) => d.country);
+
+  circles.exit().remove();
+
+  circles
     .enter()
     .append("circle")
+    .merge(circles)
+    .transition(t)
     .attr("cx", (d) => x(d.income))
     .attr("cy", (d) => y(d.life_exp))
-    // .attr("r", (d) => Math.sqrt(d.population / Math.PI));
-    .attr("r", 4);
-});
+    .attr("r", (d) => r(Math.sqrt(d.population / Math.PI)))
+    .attr("fill", (d) => color(d.continent));
+}
